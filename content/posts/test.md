5ccf6e09c566ed3ad026d1f9c21d422a136d6002
KimSia Sim <kimsia@oppoin.com>
2019-09-05 12:54:28 +0800

    make the changes for the nehalem gatsby theme to work

---
+++ content/posts/test.md
@@ -0,0 +1,9 @@
+---
+title: "Post title"
+path: "/path-to-your-post"
+tags: ["Theme"]
+featuredImage: "./cover.png"
+excerpt: Descriptive description.
+created: 2019-09-03
+updated: 2019-09-03
+---
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,26 @@
 module.exports = {
   plugins: [
-    {
-      resolve: `gatsby-theme-blog`,
-      options: {},
-    },
+  {
+    resolve: `@nehalist/gatsby-theme-nehalem`,
+    options: { // optional theme options
+        // location to our content
+        contentPath: `content`,
+        // the page manifest
+        manifest: {
+        name: `nehalem - A Gatsby theme`,
+        short_name: `nehalem`,
+        start_url: `/`,
+        background_color: `#a4cbb8`,
+        theme_color: `#a4cbb8`,
+        display: `minimal-ui`,
+        icon: `${__dirname}/assets/nehalist-gatsby.png`
+        },
+        // if archive pages should be generated automatically
+        loadDefaultPages: true,
+        // posts shown on the front page
+        postsPerPage: 5
+    }
+  }
   ],
   // Customize your site metadata:
   siteMetadata: {
